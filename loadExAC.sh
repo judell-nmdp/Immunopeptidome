66795a71aee54171b253277d2e3de43823d9e527
judell-nmdp <judell@nmdp.org>
2017-03-10 15:34:13 -0600

    Update loadExAC.sh

--- loadExAC.sh
+++ loadExAC.sh
@@ -6,4 +6,4 @@ hadoop fs -mkdir -p /data
 hadoop fs -mkdir -p /tmp
  
 wget -P /tmp ftp://ftp.broadinstitute.org/pub/ExAC_release/release1/ExAC.r1.sites.vep.vcf.gz
-hadoop fs -cat /tmp/ExAC.r1.sites.vep.vcf.gz | tabix -p vcf  | hadoop fs -put - 
+hadoop fs -cat /tmp/ExAC.r1.sites.vep.vcf.gz | tabix -p vcf  | hadoop fs -put - /data
